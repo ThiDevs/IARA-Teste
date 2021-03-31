6b635c9b5dfa9c388c37a26f1d42bd4d2ffd1180
Thiago Felicio Alves <thiago.alves@phidelis.com.br>
2021-03-31 04:11:07 -0300

    Teste Unitarios

---
+++ IARA TesteTests/Controllers/CotacaoControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IARA_Teste.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Sdk;
+
+namespace IARA_Teste.Controllers.Tests
+{
+    [TestClass()]
+    public class CotacaoControllerTests
+    {
+        [TestMethod()]
+        public void DeleteAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PosttAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PutAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAsyncTest1()
+        {
+
+        }
+    }
+}
